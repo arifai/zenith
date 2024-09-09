58235014e1bfcd87d3e47e6cb94db6e3932ccb18
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-09-09 23:37:18 +0700

    feat(config): add `config.go` to load .env file

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"github.com/Netflix/go-env"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+// Config struct to store environment variables
+type Config struct {
+	DatabaseHost     string `env:"DB_HOST"`
+	DatabasePort     string `env:"DB_PORT"`
+	DatabaseName     string `env:"DB_NAME"`
+	DatabaseUser     string `env:"DB_USER"`
+	DatabasePassword string `env:"DB_PASSWORD"`
+	SslMode          string `env:"SSL_MODE"`
+	Timezone         string `env:"TIMEZONE"`
+}
+
+// Load function to load environment variables
+func Load(filenames ...string) (config Config) {
+	if err := godotenv.Load(filenames...); err != nil {
+		log.Fatalf("Error loading `.env` file: %v", err)
+	}
+
+	_, envErr := env.UnmarshalFromEnviron(&config)
+	if envErr != nil {
+		log.Fatalf("Failed to load environment variables: %v", envErr)
+	}
+
+	return config
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,19 @@
 module github.com/arifai/go-modular-monolithic
+
+go 1.23.1
+
+require (
+	github.com/Netflix/go-env v0.1.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gorm.io/driver/postgres v1.5.9 // indirect
+	gorm.io/gorm v1.25.12 // indirect
+)
