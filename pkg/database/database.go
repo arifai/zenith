7d554c0e1428c4e6d7d717b7fde7ee65345e13cc
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-09-09 23:39:47 +0700

    feat(database): add database connection

---
+++ pkg/database/database.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"fmt"
+	"github.com/arifai/go-modular-monolithic/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"time"
+)
+
+// ConnectDatabase function to connect to database
+func ConnectDatabase(config config.Config) *gorm.DB {
+	log.Println("Connecting to database...")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s", config.DatabaseHost, config.DatabaseUser, config.DatabasePassword, config.DatabaseName, config.DatabasePort, config.SslMode, config.Timezone)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	sqlDb, dbErr := db.DB()
+	if dbErr != nil {
+		log.Fatalf("Failed to connect to database: %v", dbErr)
+	}
+
+	sqlDb.SetMaxIdleConns(10)
+	sqlDb.SetMaxIdleConns(100)
+	sqlDb.SetConnMaxLifetime(1 * time.Hour)
+
+	return db
+}
