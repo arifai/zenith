82df104ca36f1985d8bf8f7245a617c7cc34a9d2
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-09-11 00:16:33 +0700

    fix(database): load location timezone from config

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -14,9 +14,14 @@ import (
 func ConnectDatabase(config config.Config) *gorm.DB {
 	log.Println("Connecting to database...")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s", config.DatabaseHost, config.DatabaseUser, config.DatabasePassword, config.DatabaseName, config.DatabasePort, config.SslMode, config.Timezone)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", config.DatabaseHost, config.DatabaseUser, config.DatabasePassword, config.DatabaseName, config.DatabasePort, config.SslMode)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
+		NowFunc: func() time.Time {
+			ti, _ := time.LoadLocation(config.Timezone)
+
+			return time.Now().In(ti)
+		},
 	})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
