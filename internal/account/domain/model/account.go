1b28a010fc0dc565c9d39fa31cb41f5b2d93f441
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-09-11 00:17:49 +0700

    BREAKING CHANGE: use UUIDv4 for primary key

--- internal/account/domain/model/account.go
+++ internal/account/domain/model/account.go
@@ -1,38 +1,72 @@
 package model
 
 import (
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
 )
 
 // Account is a struct that represent the account model
 type Account struct {
-	ID             int64           `json:"id" gorm:"primaryKey;autoIncrement;"`
+	ID             uuid.UUID       `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
 	FullName       string          `json:"full_name" gorm:"not null;column:full_name;type:varchar"`
 	Email          string          `json:"email" gorm:"not null;column:email;type:varchar;uniqueIndex:idx_account_email"`
 	Avatar         string          `json:"avatar" gorm:"column:avatar;type:varchar"`
 	Active         bool            `json:"active" gorm:"column:active;type:boolean;default:false"`
 	FcmToken       string          `json:"fcm_token" gorm:"column:fcm_token;type:varchar"`
 	CreatedAt      time.Time       `json:"created_at" gorm:"column:created_at;autoCreateTime;default:CURRENT_TIMESTAMP"`
-	UpdatedAt      time.Time       `json:"updated_at" gorm:"column:updated_at;autoUpdateTime"`
+	UpdatedAt      *time.Time      `json:"updated_at" gorm:"column:updated_at;autoUpdateTime;default:null"`
 	UserPassHashed *UserPassHashed `gorm:"foreignKey:AccountId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 // UserPassHashed is a struct that represent the user_pass_hashed model
 type UserPassHashed struct {
-	ID         int64     `json:"id" gorm:"primaryKey;autoIncrement"`
-	AccountId  int64     `json:"account_id" gorm:"column:account_id;type:bigint"`
-	PassHashed string    `json:"pass_hashed" gorm:"not null;column:pass_hashed;type:varchar"`
-	CreatedAt  time.Time `json:"created_at" gorm:"column:created_at;autoCreateTime;default:CURRENT_TIMESTAMP"`
-	UpdatedAt  time.Time `json:"updated_at" gorm:"column:updated_at;autoUpdateTime"`
+	ID         uuid.UUID  `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	AccountId  uuid.UUID  `json:"account_id" gorm:"not null;column:account_id;type:uuid"`
+	PassHashed string     `json:"pass_hashed" gorm:"not null;column:pass_hashed;type:varchar"`
+	CreatedAt  time.Time  `json:"created_at" gorm:"column:created_at;autoCreateTime;default:CURRENT_TIMESTAMP"`
+	UpdatedAt  *time.Time `json:"updated_at" gorm:"column:updated_at;autoUpdateTime;default:null"`
 }
 
-// CreateAccount will create a new account
+func (account *Account) BeforeCreate() (err error) {
+	account.ID = uuid.New()
+
+	return
+}
+
+func (userPassHashed *UserPassHashed) BeforeCreate() (err error) {
+	userPassHashed.ID = uuid.New()
+
+	return
+}
+
+// CreateAccount creates a new account and handles its association with UserPassHashed
 func (account *Account) CreateAccount(db *gorm.DB) (*Account, error) {
-	err := db.FirstOrCreate(&account, Account{Email: account.Email}).Error
-	if err := db.Model(&account).Association("UserPassHashed").Find(&account.UserPassHashed); err != nil {
+	tx := db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := tx.FirstOrCreate(account, Account{Email: account.Email}).Error; err != nil {
+		tx.Rollback()
+
 		return nil, err
 	}
 
-	return account, err
+	if account.UserPassHashed != nil {
+		account.UserPassHashed.AccountId = account.ID
+		if err := tx.Create(account.UserPassHashed).Error; err != nil {
+			tx.Rollback()
+
+			return nil, err
+		}
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		return nil, err
+	}
+
+	return account, nil
 }
--- internal/account/domain/model/account_migration.go
+++ internal/account/domain/model/account_migration.go
@@ -2,41 +2,54 @@ package model
 
 import (
 	"github.com/arifai/go-modular-monolithic/pkg/crypto"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"log"
 )
 
 // AccountMigration will create the account table and insert the initial data
 func AccountMigration(db *gorm.DB) {
-	err := db.Debug().AutoMigrate(&Account{}, &UserPassHashed{})
+	err := db.Transaction(func(tx *gorm.DB) error {
+		err := tx.Debug().AutoMigrate(&Account{}, &UserPassHashed{})
+		if err != nil {
+			return err
+		}
+
+		account := &Account{
+			ID:       uuid.New(),
+			FullName: "John Doe",
+			Email:    "john.doe@mail.com",
+			Avatar:   "https://api.dicebear.com/9.x/notionists/png?scale=130&size=260&backgroundColor=b6e3f4",
+			Active:   true,
+		}
+
+		p := crypto.Argon2IdHash{Time: 1, Memory: 64 * 1024, Threads: 4, KeyLen: 32, SaltLen: 16}
+		hashSalt, err := crypto.GenerateHash(p, []byte("12345678"), nil)
+		if err != nil {
+			return err
+		}
+
+		userPassHashed := &UserPassHashed{
+			AccountId:  account.ID,
+			PassHashed: hashSalt,
+		}
+
+		if tx.Model(&account).Where("email = ?", account.Email).Updates(&account).RowsAffected == 0 {
+			if err := tx.Create(&account).Error; err != nil {
+				return err
+			}
+		}
+
+		if tx.Model(&userPassHashed).Where("account_id = ?", account.ID).Updates(&userPassHashed).RowsAffected == 0 {
+			if err := tx.Create(&userPassHashed).Error; err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+
 	if err != nil {
-		log.Fatalf("Error migrating account model: %v", err)
-	}
-
-	account := &Account{
-		ID:       1,
-		FullName: "John Doe",
-		Email:    "john.doe@mail.com",
-		Avatar:   "https://api.dicebear.com/9.x/notionists/png?scale=130&size=260&backgroundColor=b6e3f4",
-		Active:   true,
-	}
-
-	p := crypto.Argon2IdHash{Time: 1, Memory: 64 * 1024, Threads: 4, KeyLen: 32, SaltLen: 16}
-	hashSalt, err := crypto.GenerateHash(p, []byte("12345678"), nil)
-	if err != nil {
-		log.Fatalf("Error generating hash: %v", err)
-	}
-
-	passHashed := &UserPassHashed{
-		AccountId:  account.ID,
-		PassHashed: hashSalt,
-	}
-
-	if db.Model(&account).Where("email = ?", account.Email).Updates(&account).RowsAffected == 0 {
-		db.Create(&account)
-	}
-
-	if db.Model(&passHashed).Where("account_id = ?", account.ID).Updates(&passHashed).RowsAffected == 0 {
-		db.Create(&passHashed)
+		log.Fatalf("Error during account migration: %v", err)
 	}
 }
