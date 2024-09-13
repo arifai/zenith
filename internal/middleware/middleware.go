ae2b78773842f23033f5c5fff0f52ce3ecedde31
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-09-13 15:47:23 +0700

    feat(middleware): add authentication middleware

---
+++ internal/middleware/middleware.go
@@ -0,0 +1,58 @@
+package middleware
+
+import (
+	"github.com/arifai/go-modular-monolithic/config"
+	"github.com/arifai/go-modular-monolithic/internal/account/domain/repository"
+	errmsg "github.com/arifai/go-modular-monolithic/internal/errors"
+	"github.com/arifai/go-modular-monolithic/pkg/common"
+	crp "github.com/arifai/go-modular-monolithic/pkg/crypto"
+	"github.com/arifai/go-modular-monolithic/pkg/utils"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"strings"
+)
+
+// Middleware function to check authorization header
+func Middleware(db *gorm.DB) gin.HandlerFunc {
+	repo := repository.NewAccountRepository(db)
+
+	return func(c *gin.Context) {
+		resp := common.Response{}
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			resp.Unauthorized(c, []utils.IError{}, errmsg.ErrMissingAuthorizationHeaderText)
+			c.Abort()
+			return
+		}
+
+		tokenParts := strings.Split(authHeader, " ")
+		if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+			resp.Unauthorized(c, []utils.IError{}, errmsg.ErrInvalidAccessTokenText)
+			c.Abort()
+			return
+		}
+
+		tokenString := tokenParts[1]
+		tokenPayload, err := crp.VerifyToken(tokenString, config.PublicKey)
+		if err != nil {
+			resp.Unauthorized(c, []utils.IError{}, err.Error())
+			c.Abort()
+			return
+		}
+		if tokenPayload.TokenType != "access_token" {
+			resp.Unauthorized(c, []utils.IError{}, errmsg.ErrInvalidTokenTypeText)
+			c.Abort()
+			return
+		}
+
+		account, err := repo.Find(tokenPayload.AccountId)
+		if err != nil {
+			resp.Unauthorized(c, []utils.IError{}, errmsg.ErrCannotFindAuthorizedAccountText)
+			c.Abort()
+			return
+		}
+
+		c.Set("account", account)
+		c.Next()
+	}
+}
