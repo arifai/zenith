e83e87c79ae558cf1818e0c796703e3552fef461
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-09-13 18:43:25 +0700

    refactor(utils): remove underscore in the string
    Signed-off-by: Ahmad Rifa'i <personal.rifai@gmail.com>

--- pkg/utils/capitalize_first_letter.go
+++ pkg/utils/capitalize_first_letter.go
@@ -2,11 +2,19 @@ package utils
 
 import "strings"
 
-// CapitalizeFirstLetter is a function to capitalize first letter of a string
+// CapitalizeFirstLetter is a function to capitalize first letter of a string.
+// This function will split the string by underscore, and join them back with space.
 func CapitalizeFirstLetter(text string) string {
 	if len(text) == 0 {
 		return text
 	}
 
-	return strings.ToUpper(string(text[0])) + text[1:]
+	words := strings.Split(text, "_")
+	for i, word := range words {
+		if len(word) > 0 {
+			words[i] = strings.ToUpper(string(word[0])) + word[1:]
+		}
+	}
+
+	return strings.Join(words, " ")
 }
