934fd66a6c880e1f44d89914275b26c17b3549d6
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-09-15 11:05:31 +0700

    refactor(account): improved clarity & reusability methods
    Co-authored-by: Ahmad Rifa'i <personal.rifai@gmail.com>

--- internal/account/domain/repository/account_repository.go
+++ internal/account/domain/repository/account_repository.go
@@ -5,23 +5,25 @@ import (
 	"github.com/arifai/go-modular-monolithic/config"
 	"github.com/arifai/go-modular-monolithic/internal/account/api/types"
 	"github.com/arifai/go-modular-monolithic/internal/account/domain/model"
-	crp "github.com/arifai/go-modular-monolithic/pkg/crypto"
+	"github.com/arifai/go-modular-monolithic/pkg/crypto"
 	"github.com/arifai/go-modular-monolithic/pkg/errormessage"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
+var passwordSalt = config.Load().PasswordSalt
+
 // AccountRepository handles CRUD operations for account data in the database.
-type AccountRepository struct{ db *gorm.DB }
+type AccountRepository struct {
+	db *gorm.DB
+}
 
 // NewAccountRepository initializes a new AccountRepository with a database context and debug mode enabled.
 func NewAccountRepository(db *gorm.DB) *AccountRepository {
 	return &AccountRepository{db: db.WithContext(context.Background()).Debug()}
 }
 
-// CreateAccount registers a new user account in the system using the provided payload data. The payload must contain
-// full name, email, and password. If the email already exists in the database, it returns an errormessage.ErrEmailAlreadyExists error.
-// Passwords are hashed using Argon2ID hashing algorithm before saving. Returns the created model.Account or any errormessage encountered.
+// CreateAccount registers a new user account in the system using the provided payload data.
 func (repo *AccountRepository) CreateAccount(payload *types.AccountCreateRequest) (*model.Account, error) {
 	m := new(model.Account)
 	exists, err := m.EmailExists(repo.db, payload.Email)
@@ -31,79 +33,75 @@ func (repo *AccountRepository) CreateAccount(payload *types.AccountCreateRequest
 		return nil, errormessage.ErrEmailAlreadyExists
 	}
 
-	generateHash, err := crp.DefaultArgon2IDHash.GenerateHash([]byte(payload.Password), []byte(config.Load().PasswordSalt))
+	hash, err := generatePasswordHash(payload.Password)
 	if err != nil {
 		return nil, err
 	}
+
 	account := &model.Account{FullName: payload.FullName, Email: payload.Email}
-	accountPassHashed := &model.AccountPassHashed{
+	account.AccountPassHashed = &model.AccountPassHashed{
 		AccountId:  account.ID,
-		PassHashed: generateHash,
+		PassHashed: hash,
 	}
-	account.AccountPassHashed = accountPassHashed
 
 	return account.CreateAccount(repo.db)
+}
+
+func generatePasswordHash(password string) (string, error) {
+	return crypto.DefaultArgon2IDHash.GenerateHash([]byte(password), []byte(passwordSalt))
 }
 
 // Find retrieves an account by its uuid.UUID from the database.
 func (repo *AccountRepository) Find(id uuid.UUID) (*model.Account, error) {
 	account := new(model.Account)
-	founded, err := account.FindByID(repo.db, id)
+	foundAccount, err := account.FindByID(repo.db, id)
 	if err != nil {
 		return nil, err
 	}
-
-	return founded, nil
+	return foundAccount, nil
 }
 
 // FindByEmail retrieves an account by its email address from the database.
-// It returns a pointer to the model.Account and an error if any.
 func (repo *AccountRepository) FindByEmail(email string) (*model.Account, error) {
 	account := new(model.Account)
-	founded, err := account.FindByEmail(repo.db, email)
+	foundAccount, err := account.FindByEmail(repo.db, email)
 	if err != nil {
 		return nil, err
 	}
-
-	return founded, nil
+	return foundAccount, nil
 }
 
 // Update modifies an existing account with the given id using the provided payload data.
-// If the account is found, its FullName and Email fields are updated and changes are saved in the database.
-// Returns the updated model.Account or an error if any step fails.
 func (repo *AccountRepository) Update(id uuid.UUID, payload *types.AccountUpdateRequest) (*model.Account, error) {
-	founded, err := repo.Find(id)
+	account, err := repo.Find(id)
 	if err != nil {
 		return nil, err
 	}
 
-	founded.FullName = payload.FullName
-	founded.Email = payload.Email
+	account.FullName = payload.FullName
+	account.Email = payload.Email
 
-	return founded.Update(repo.db)
+	return account.Update(repo.db)
 }
 
 // UpdatePassword updates the password for an existing account identified by id.
-// It verifies the old password and hashes the new password before saving it to the database.
-// Returns the updated model.Account or an error if any step fails.
 func (repo *AccountRepository) UpdatePassword(id uuid.UUID, payload *types.AccountUpdatePasswordRequest) (*model.Account, error) {
-	founded, err := repo.Find(id)
+	account, err := repo.Find(id)
 	if err != nil {
 		return nil, err
 	}
 
-	verifyHash, err := crp.VerifyHash(payload.OldPassword, founded.AccountPassHashed.PassHashed)
-	if err != nil {
-		return nil, err
-	} else if !verifyHash {
+	isMatch, err := crypto.VerifyHash(payload.OldPassword, account.AccountPassHashed.PassHashed)
+	if err != nil || !isMatch {
 		return nil, errormessage.ErrWrongOldPassword
-	} else {
-		generateHash, err := crp.DefaultArgon2IDHash.GenerateHash([]byte(payload.NewPassword), []byte(config.Load().PasswordSalt))
-		if err != nil {
-			return nil, err
-		}
-		founded.AccountPassHashed.PassHashed = generateHash
 	}
 
-	return founded.UpdatePassword(repo.db)
+	hash, err := generatePasswordHash(payload.NewPassword)
+	if err != nil {
+		return nil, err
+	}
+
+	account.AccountPassHashed.PassHashed = hash
+
+	return account.UpdatePassword(repo.db)
 }
