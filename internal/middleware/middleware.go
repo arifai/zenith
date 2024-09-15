7cfe03a8f979920e3b1b65b86101d2f4c5504245
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-09-15 10:52:52 +0700

    refactor(middleware): extract token validation and account retrieval
    Co-authored-by: Ahmad Rifa'i <personal.rifai@gmail.com>

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"errors"
 	"github.com/arifai/go-modular-monolithic/config"
+	"github.com/arifai/go-modular-monolithic/internal/account/domain/model"
 	"github.com/arifai/go-modular-monolithic/internal/account/domain/repository"
 	"github.com/arifai/go-modular-monolithic/pkg/common"
 	crp "github.com/arifai/go-modular-monolithic/pkg/crypto"
@@ -15,44 +17,51 @@ import (
 // Middleware is a Gin middleware for validating access tokens from Authorization headers and setting authorized account context.
 func Middleware(db *gorm.DB) gin.HandlerFunc {
 	repo := repository.NewAccountRepository(db)
-
-	return func(c *gin.Context) {
+	return func(ctx *gin.Context) {
 		resp := common.Response{}
-		authHeader := c.GetHeader("Authorization")
-		if authHeader == "" {
-			resp.Unauthorized(c, []utils.IError{}, errormessage.ErrMissingAuthorizationHeaderText)
-			c.Abort()
-			return
+		if account, err := validateAndExtractAccount(ctx, repo); err != nil {
+			resp.Unauthorized(ctx, []utils.IError{}, err.Error())
+			ctx.Abort()
+		} else {
+			ctx.Set("account", account)
+			ctx.Next()
 		}
-
-		tokenParts := strings.Split(authHeader, " ")
-		if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
-			resp.Unauthorized(c, []utils.IError{}, errormessage.ErrInvalidAccessTokenText)
-			c.Abort()
-			return
-		}
-
-		tokenString := tokenParts[1]
-		tokenPayload, err := crp.VerifyToken(tokenString, config.PublicKey)
-		if err != nil {
-			resp.Unauthorized(c, []utils.IError{}, err.Error())
-			c.Abort()
-			return
-		}
-		if tokenPayload.TokenType != "access_token" {
-			resp.Unauthorized(c, []utils.IError{}, errormessage.ErrInvalidTokenTypeText)
-			c.Abort()
-			return
-		}
-
-		account, err := repo.Find(tokenPayload.AccountId)
-		if err != nil {
-			resp.Unauthorized(c, []utils.IError{}, errormessage.ErrCannotFindAuthorizedAccountText)
-			c.Abort()
-			return
-		}
-
-		c.Set("account", account)
-		c.Next()
 	}
+}
+
+// validateAndExtractAccount validates the token and extracts the account information.
+func validateAndExtractAccount(ctx *gin.Context, repo *repository.AccountRepository) (*model.Account, error) {
+	authHeader := ctx.GetHeader("Authorization")
+	if authHeader == "" {
+		return nil, errors.New(errormessage.ErrMissingAuthorizationHeaderText)
+	}
+
+	tokenString, err := extractToken(authHeader)
+	if err != nil {
+		return nil, err
+	}
+
+	tokenPayload, err := crp.VerifyToken(tokenString, config.PublicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	if tokenPayload.TokenType != "access_token" {
+		return nil, errors.New(errormessage.ErrInvalidTokenTypeText)
+	}
+
+	account, err := repo.Find(tokenPayload.AccountId)
+	if err != nil {
+		return nil, errors.New(errormessage.ErrCannotFindAuthorizedAccountText)
+	}
+	return account, nil
+}
+
+// extractToken splits the authorization header to extract the token.
+func extractToken(authHeader string) (string, error) {
+	tokenParts := strings.Split(authHeader, " ")
+	if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+		return "", errors.New(errormessage.ErrInvalidAccessTokenText)
+	}
+	return tokenParts[1], nil
 }
