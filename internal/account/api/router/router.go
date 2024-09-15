daceddc5a3f9739ffe96a506d0bd174f46348cb6
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-09-15 11:11:11 +0700

    refactor(account): organized routes for better clarity and structure
    Co-authored-by: Ahmad Rifa'i <personal.rifai@gmail.com>

--- internal/account/api/router/router.go
+++ internal/account/api/router/router.go
@@ -10,23 +10,19 @@ import (
 
 // AccountRouter sets up all routes related to account operations under the provided gin.RouterGroup.
 func AccountRouter(group *gin.RouterGroup, db *gorm.DB, config *config.Config) {
-	group.Group("/auth").
-		Group("/account").
-		POST("/authorization", func(c *gin.Context) {
-			handler.AuthHandler(c, db, config)
-		}).
-		POST("/registration", func(c *gin.Context) {
-			handler.RegisterAccountHandler(c, db, config)
-		})
+	accountHandler := handler.NewAccountHandler(db, config)
 
-	group.Group("/account").
-		Use(middleware.Middleware(db)).
-		GET("/me", func(c *gin.Context) {
-			handler.GetAccountHandler(c, db, config)
-		}).
-		PATCH("/me/update", func(c *gin.Context) {
-			handler.UpdateAccountHandler(c, db, config)
-		}).PUT("/me/update_password", func(c *gin.Context) {
-		handler.UpdatePasswordAccountHandler(c, db, config)
-	})
+	authAccountGroup := group.Group("/auth/account")
+	{
+		authAccountGroup.POST("/authorization", accountHandler.AuthHandler)
+		authAccountGroup.POST("/registration", accountHandler.RegisterAccountHandler)
+	}
+
+	accountGroup := group.Group("/account")
+	accountGroup.Use(middleware.Middleware(db))
+	{
+		accountGroup.GET("/me", accountHandler.GetAccountHandler)
+		accountGroup.PATCH("/me/update", accountHandler.UpdateAccountHandler)
+		accountGroup.PUT("/me/update_password", accountHandler.UpdatePasswordAccountHandler)
+	}
 }
