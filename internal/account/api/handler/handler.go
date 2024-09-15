4ca094c5044f1208f4b1f72b657e52487ac6461e
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-09-15 11:08:39 +0700

    refactor(account): use `AccountHandler` struct for improved organization
    Co-authored-by: Ahmad Rifa'i <personal.rifai@gmail.com>

--- internal/account/api/handler/handler.go
+++ internal/account/api/handler/handler.go
@@ -11,101 +11,110 @@ import (
 	"gorm.io/gorm"
 )
 
-// AuthHandler handles the authentication requests by validating input, using the account service to authorize,
-// and responding accordingly.
-func AuthHandler(ctx *gin.Context, db *gorm.DB, config *config.Config) {
-	resp := new(common.Response)
-	accountService := service.NewAccountAuthService(db, config)
+// AccountHandler manages account-related operations such as authentication, retrieval, registration, and updates.
+// It uses a gorm.DB instance for database operations, config.Config for configuration settings, and common.Response for responses.
+type AccountHandler struct {
+	db     *gorm.DB
+	config *config.Config
+	resp   *common.Response
+}
+
+// NewAccountHandler initializes an AccountHandler with a given database connection and configuration settings.
+func NewAccountHandler(db *gorm.DB, config *config.Config) *AccountHandler {
+	return &AccountHandler{
+		db:     db,
+		config: config,
+		resp:   new(common.Response),
+	}
+}
+
+// AuthHandler handles the authentication logic for user accounts, including request validation and response formatting.
+func (a *AccountHandler) AuthHandler(ctx *gin.Context) {
+	accountService := service.NewAccountAuthService(a.db, a.config)
 	body, err := utils.ValidateBody[types.AccountAuthRequest](ctx)
 	if err != nil {
-		resp.Error(ctx, err)
+		a.resp.Error(ctx, err)
 		return
 	}
 
 	result, err := accountService.Authorize(body)
 	if err != nil {
-		resp.Error(ctx, err)
+		a.resp.Error(ctx, err)
 		return
 	}
 
-	resp.Authorized(ctx, result)
+	a.resp.Authorized(ctx, result)
 }
 
-// GetAccountHandler handles the retrieval of an account using the GetAccount service.
-// It initializes the account service and context, then attempts to get the account.
-// Upon success, it sends a success response; otherwise, it sends an error response.
-func GetAccountHandler(ctx *gin.Context, db *gorm.DB, config *config.Config) {
-	resp := new(common.Response)
-	accountService := service.NewAccountService(db, config)
+// GetAccountHandler retrieves the current user's account details. It initializes account service and context,
+// fetches account information, and sends a success or error response based on the result.
+func (a *AccountHandler) GetAccountHandler(ctx *gin.Context) {
+	accountService := service.NewAccountService(a.db, a.config)
 	context := core.NewContext(ctx)
 	result, err := accountService.GetAccount(context)
 	if err != nil {
-		resp.Error(ctx, err)
+		a.resp.Error(ctx, err)
 		return
 	}
 
-	resp.Success(ctx, result)
+	a.resp.Success(ctx, result)
 }
 
-// RegisterAccountHandler handles the registration of a new user account.
-// It validates the request body, creates a new user account using the CreateAccount service,
-// and returns an appropriate response based on the operation outcome.
-func RegisterAccountHandler(ctx *gin.Context, db *gorm.DB, config *config.Config) {
-	resp := new(common.Response)
-	accountService := service.NewAccountService(db, config)
+// RegisterAccountHandler handles the registration of a new user account by validating the request
+// body, invoking the account service to create the account, and sending an appropriate response.
+func (a *AccountHandler) RegisterAccountHandler(ctx *gin.Context) {
+	accountService := service.NewAccountService(a.db, a.config)
 	body, err := utils.ValidateBody[types.AccountCreateRequest](ctx)
 	if err != nil {
-		resp.Error(ctx, err)
+		a.resp.Error(ctx, err)
 		return
 	}
 
 	result, err := accountService.CreateAccount(body)
 	if err != nil {
-		resp.Error(ctx, err)
+		a.resp.Error(ctx, err)
 		return
 	}
 
-	resp.Created(ctx, "", result)
+	a.resp.Created(ctx, "", result)
 }
 
-// UpdateAccountHandler handles the HTTP request for updating an account.
-// It validates the request body, invokes the account service to update the account,
-// and sends the appropriate response based on the success or failure of the update operation.
-func UpdateAccountHandler(ctx *gin.Context, db *gorm.DB, config *config.Config) {
-	resp := new(common.Response)
-	accountService := service.NewAccountService(db, config)
+// UpdateAccountHandler handles the updating of user account information. It validates the request body, updates
+// the account via the account service, and sends a success or error response based on the result.
+func (a *AccountHandler) UpdateAccountHandler(ctx *gin.Context) {
+	accountService := service.NewAccountService(a.db, a.config)
 	context := core.NewContext(ctx)
 	body, err := utils.ValidateBody[types.AccountUpdateRequest](ctx)
 	if err != nil {
-		resp.Error(ctx, err)
+		a.resp.Error(ctx, err)
 		return
 	}
 
 	result, err := accountService.UpdateAccount(context, body)
 	if err != nil {
-		resp.Error(ctx, err)
+		a.resp.Error(ctx, err)
 		return
 	}
 
-	resp.Success(ctx, result)
+	a.resp.Success(ctx, result)
 }
 
-// UpdatePasswordAccountHandler handles the request to update an account password using provided old and new passwords.
-func UpdatePasswordAccountHandler(ctx *gin.Context, db *gorm.DB, config *config.Config) {
-	resp := new(common.Response)
-	accountService := service.NewAccountService(db, config)
+// UpdatePasswordAccountHandler handles the password update request for an account.
+// It validates the request body, calls the account service to update the password, and sends the appropriate response.
+func (a *AccountHandler) UpdatePasswordAccountHandler(ctx *gin.Context) {
+	accountService := service.NewAccountService(a.db, a.config)
 	context := core.NewContext(ctx)
 	body, err := utils.ValidateBody[types.AccountUpdatePasswordRequest](ctx)
 	if err != nil {
-		resp.Error(ctx, err)
+		a.resp.Error(ctx, err)
 		return
 	}
 
 	result, err := accountService.UpdatePassword(context, body)
 	if err != nil {
-		resp.Error(ctx, err)
+		a.resp.Error(ctx, err)
 		return
 	}
 
-	resp.Success(ctx, result)
+	a.resp.Success(ctx, result)
 }
