f56b862c2a26b58921c48dcf91bc6f51d0bdeae4
Ahmad Rifa'i <personal.rifai@gmail.com>
2024-10-10 09:09:43 +0700

    fix(account): fix error in FindByEmail method

--- internal/repository/account.go
+++ internal/repository/account.go
@@ -45,13 +45,13 @@ func (a *accountRepository) Create(account *model.Account) error {
 }
 
 func (a *accountRepository) FindByEmail(email string) (*model.Account, error) {
-	var account model.Account
+	account := &model.Account{}
 	if err := a.db.Where(&model.Account{Email: email}).Preload("AccountPassHashed").
 		First(account).Error; err != nil {
 		return nil, err
 	}
 
-	return &account, nil
+	return account, nil
 }
 
 func (a *accountRepository) FindByID(id *uuid.UUID) (*model.Account, error) {
